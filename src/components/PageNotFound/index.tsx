e98dba3ac9ff5045339aee7d24eb76abdd114750
Luiz Henrique <7henrique18@gmail.com>
2021-07-03 15:13:41 -0300

    :gear: Fix layout

--- src/components/PageNotFound/index.tsx
+++ src/components/PageNotFound/index.tsx
@@ -3,8 +3,10 @@ export function PageNotFound() {
   return (
     <div className="pageNotFound">
       <img src="./404.png" alt="" />
-      <h1>We didn&apos;t find anything 😔</h1>
-      <p>but we wish you peace amoung the worlds on your journey! 🖕</p>
+      <div>
+        <h1>We didn&apos;t find anything 😔</h1>
+        <p>but we wish you peace amoung the worlds on your journey! 🖕</p>
+      </div>
     </div>
   );
 }
